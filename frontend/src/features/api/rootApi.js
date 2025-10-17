1709c9520d3c6e43a083ba42d9f9432a4cc681bf
seenumedio <srinivas65888@gmail.com>
2025-10-18 01:09:45 +0530

    backend deployed

--- frontend/src/features/api/rootApi.js
+++ frontend/src/features/api/rootApi.js
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 export const rootApi = createApi({
     reducerPath: 'api',
     baseQuery: fetchBaseQuery({
-        baseUrl: 'http://localhost:4000/api'
+        baseUrl: 'https://nestcom.onrender.com/api'
     }),
     tagTypes: [''],
     endpoints: (builder)=>({})
--- frontend/src/features/auth/authApi.js
+++ frontend/src/features/auth/authApi.js
@@ -1,5 +1,5 @@
 export const registerUser = async (username, email, password) => {
-  const res = await fetch('http://localhost:4000/api/auth/register', {
+  const res = await fetch('https://nestcom.onrender.com/api/auth/register', {
     method: 'POST',
     headers: { 'Content-Type': 'application/json' },
     body: JSON.stringify({ username, email, password })
@@ -17,7 +17,7 @@ export const registerUser = async (username, email, password) => {
 };
 
 export const loginUser = async (email, password) => {
-  const res = await fetch('http://localhost:4000/api/auth/login', {
+  const res = await fetch('https://nestcom.onrender.com/api/auth/login', {
     method: 'POST',
     headers: { 'Content-Type': 'application/json' },
     body: JSON.stringify({ email, password }),
